20b49f5b826457e90714fc69f72ff15ca53d2edf
Robert Hamel <roberthamel@gmail.com>
2018-04-06 15:20:31 -0400

    initial commit

---
+++ src/main/java/com/revature/hydraaggregatorservice/HydraAggregatorServiceApplication.java
@@ -0,0 +1,12 @@
+package com.revature.hydraaggregatorservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HydraAggregatorServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HydraAggregatorServiceApplication.class, args);
+	}
+}
