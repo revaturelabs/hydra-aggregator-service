1647571d3446637ac04e95db183fe49315143d2e
Robert Hamel <roberthamel@gmail.com>
2018-04-07 17:30:57 -0400

    add swagger docs

--- src/main/java/com/revature/hydraaggregatorservice/HydraAggregatorServiceApplication.java
+++ src/main/java/com/revature/hydraaggregatorservice/HydraAggregatorServiceApplication.java
@@ -2,11 +2,25 @@ package com.revature.hydraaggregatorservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableJpaRepositories
+@EnableDiscoveryClient
+@EnableSwagger2
 public class HydraAggregatorServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HydraAggregatorServiceApplication.class, args);
+	}
+
+	@Bean
+	public Docket api() {
+		return new Docket(DocumentationType.SWAGGER_2);
 	}
 }
