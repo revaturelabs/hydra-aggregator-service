c263f371ed30aca534bd414fd8b92a4d90b71101
Robert Hamel <roberthamel@gmail.com>
2018-04-09 12:01:12 -0400

    disable csrf check

--- src/main/java/com/revature/hydraaggregatorservice/config/SecurityConfig.java
+++ src/main/java/com/revature/hydraaggregatorservice/config/SecurityConfig.java
@@ -12,6 +12,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.cors().configurationSource(request -> new CorsConfiguration().applyPermitDefaultValues());
+        http.cors().configurationSource(request -> new CorsConfiguration().applyPermitDefaultValues())
+        .and()
+        .csrf().disable();
     }
 }
