9acf783645ff6404acacfbefebff5232f7a051c3
Robert Hamel <roberthamel@gmail.com>
2018-04-09 15:00:29 -0400

    cors fix attempt

--- src/main/java/com/revature/hydraaggregatorservice/controller/BatchController.java
+++ src/main/java/com/revature/hydraaggregatorservice/controller/BatchController.java
@@ -17,7 +17,6 @@ public class BatchController {
         this.batchRepository = batchRepository;
     }
 
-    @CrossOrigin(exposedHeaders = "Access-Control-Allow-Origin")
     @RequestMapping(value = "/batches", method = RequestMethod.GET)
     public ResponseEntity getBatches() {
         return new ResponseEntity(batchRepository.findAll(), HttpStatus.OK);
--- src/main/java/com/revature/hydraaggregatorservice/controller/CurriculumController.java
+++ src/main/java/com/revature/hydraaggregatorservice/controller/CurriculumController.java
@@ -20,13 +20,11 @@ public class CurriculumController {
         this.focusRepository = focusRepository;
     }
 
-    @CrossOrigin(exposedHeaders = "Access-Control-Allow-Origin")
     @RequestMapping(value = "/curricula", method = RequestMethod.GET)
     public ResponseEntity getCurricula() {
         return new ResponseEntity(curriculumRepository.findAll(), HttpStatus.OK);
     }
 
-    @CrossOrigin(exposedHeaders = "Access-Control-Allow-Origin")
     @RequestMapping(value = "/focuses", method = RequestMethod.GET)
     public ResponseEntity getFocuses() {
         return new ResponseEntity(focusRepository.findAll(), HttpStatus.OK);
